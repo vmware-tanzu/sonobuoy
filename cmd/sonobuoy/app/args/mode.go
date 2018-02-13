888266c80b912d471975353648ca9bc7681dd2be
Liz <33235082+liztio@users.noreply.github.com>
2018-02-13 13:36:15 -0500

    Sonobuoy run' (#242)
    * sonobuoy run applies the config from `sonobuoy gen` to a cluster
    * Remove unneeded dependencies
    * Move getting kubeconfig into run.go
    * Utils -> Args
    * Code review feedback
    
    Signed-off-by: liz <liz@heptio.com>
    
    Signed-off-by: Jesse Hamilton jesse.hamilton@heptio.com

--- cmd/sonobuoy/app/args/mode.go
+++ cmd/sonobuoy/app/args/mode.go
@@ -1,4 +1,4 @@
-package mode
+package args
 
 import (
 	"fmt"
@@ -7,45 +7,43 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// Name is the name of the mode
-
 const (
 	// Quick runs a single E2E test and the systemd log tests
-	Quick Name = "quick"
+	Quick Mode = "quick"
 	// Conformance runs all of the E2E tests and the systemd log tests
-	Conformance Name = "conformance"
+	Conformance Mode = "conformance"
 	// Extended run all of the E2E tests, the systemd log tests, and
 	// Heptio's E2E Tests
-	Extended Name = "extended"
+	Extended Mode = "extended"
 )
 
-// Name identifies a specific mode
-type Name string
+// Mode identifies a specific mode
+type Mode string
 
-// Config is the
-type Config struct {
+// ModeConfig represents the sonobuoy configuration for a given mode
+type ModeConfig struct {
 	// E2EFocus is the string to be passed to the E2EFOCUS env var
 	E2EFocus string
 	// Selectors are the plugins selected by thi mode
 	Selectors []plugin.Selection
 }
 
-// AddFlag adds a mode flag to existing command
-func AddFlag(modeName *Name, cmd *cobra.Command) {
+// AddModeFlag adds a mode flag to existing command
+func AddModeFlag(modeMode *Mode, cmd *cobra.Command) {
 	cmd.PersistentFlags().VarP(
-		modeName, "mode", "m",
+		modeMode, "mode", "m",
 		"What mode to run sonobuoy in",
 	)
 }
 
 // String needed for pflag.Value
-func (n *Name) String() string { return string(*n) }
+func (n *Mode) String() string { return string(*n) }
 
 // Type needed for pflag.Value
-func (n *Name) Type() string { return "Name" }
+func (n *Mode) Type() string { return "Mode" }
 
 // Set the name with a given string. Returns error on unknown mode
-func (n *Name) Set(str string) error {
+func (n *Mode) Set(str string) error {
 	switch str {
 	case "conformance":
 		*n = Conformance
@@ -59,9 +57,9 @@ func (n *Name) Set(str string) error {
 	return nil
 }
 
-// Get returns the Config associated with a mode name, or nil
+// Get returns the ModeConfig associated with a mode name, or nil
 // if there's no associated mode
-func (n *Name) Get() *Config {
+func (n *Mode) Get() *ModeConfig {
 	// Default value
 	name := Conformance
 	if n != nil && *n != "" {
@@ -69,7 +67,7 @@ func (n *Name) Get() *Config {
 	}
 	switch name {
 	case Conformance:
-		return &Config{
+		return &ModeConfig{
 			E2EFocus: "Conformance",
 			Selectors: []plugin.Selection{
 				{Name: "e2e"},
@@ -77,7 +75,7 @@ func (n *Name) Get() *Config {
 			},
 		}
 	case Quick:
-		return &Config{
+		return &ModeConfig{
 			E2EFocus: "Pods should be submitted and removed",
 			Selectors: []plugin.Selection{
 				{Name: "e2e"},
@@ -85,7 +83,7 @@ func (n *Name) Get() *Config {
 			},
 		}
 	case Extended:
-		return &Config{
+		return &ModeConfig{
 			E2EFocus: "Conformance",
 			Selectors: []plugin.Selection{
 				{Name: "e2e"},
