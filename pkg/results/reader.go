6b6d54ed9cd0ba3e80a15305a075b5a6ade190c2
Chuck Ha <chuck@heptio.com>
2018-02-27 11:56:41 -0500

    Implements e2e subcommand
    This subcommand allows users to more easily inspect their
    results archive. They can see which tests passed, failed or
    were skipped. This also allows users to rerun the tests that
    failed.
    
    Closes #248
    
    Signed-off-by: Chuck Ha <chuck@heptio.com>
    
    Signed-off-by: Jesse Hamilton jesse.hamilton@heptio.com

--- pkg/results/reader.go
+++ pkg/results/reader.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2018 Heptio Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package results
 
 import (
@@ -16,17 +32,18 @@ import (
 )
 
 const (
+	// PluginsDir defines where in the archive the plugin results are.
+	PluginsDir = "plugins/"
+
 	hostsDir                  = "hosts/"
 	namespacedResourcesDir    = "resources/ns/"
 	nonNamespacedResourcesDir = "resources/cluster/"
-	pluginsDir                = "plugins/"
 	podLogs                   = "podlogs/"
 	metadataDir               = "meta/"
-
-	defaultServicesFile      = "Services.json"
-	defaultNodesFile         = "Nodes.json"
-	defaultServerVersionFile = "serverversion.json"
-	defaultServerGroupsFile  = "servergroups.json"
+	defaultServicesFile       = "Services.json"
+	defaultNodesFile          = "Nodes.json"
+	defaultServerVersionFile  = "serverversion.json"
+	defaultServerGroupsFile   = "servergroups.json"
 )
 
 const (
