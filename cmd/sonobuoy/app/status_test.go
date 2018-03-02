2e0ed6ce67973476eb6afdb2a350229739f71929
liz <liz@heptio.com>
2018-03-02 16:56:51 -0500

    --show-all for status
    Signed-off-by: liz <liz@heptio.com>

---
+++ cmd/sonobuoy/app/status_test.go
@@ -0,0 +1,85 @@
+package app
+
+import (
+	"bytes"
+	"io"
+	"testing"
+
+	"github.com/heptio/sonobuoy/pkg/plugin/aggregation"
+)
+
+var expectedSummary = `PLUGIN		STATUS		COUNT
+e2e		complete	1
+systemd_logs	complete	1
+systemd_logs	running		2
+
+Sonobuoy is still running. Runs can take up to 60 minutes.
+`
+
+var expectedShowAll = `PLUGIN		NODE	STATUS
+e2e			complete
+systemd_logs	node01	running
+systemd_logs	node02	complete
+systemd_logs	node03	running
+
+Sonobuoy is still running. Runs can take up to 60 minutes.
+`
+
+var exampleStatus = aggregation.Status{
+	Status: "running",
+	Plugins: []aggregation.PluginStatus{
+		{
+			Plugin: "e2e",
+			Node:   "",
+			Status: "complete",
+		},
+		{
+			Plugin: "systemd_logs",
+			Node:   "node01",
+			Status: "running",
+		},
+		{
+			Plugin: "systemd_logs",
+			Node:   "node02",
+			Status: "complete",
+		},
+		{
+			Plugin: "systemd_logs",
+			Node:   "node03",
+			Status: "running",
+		},
+	},
+}
+
+func TestPrintStatus(t *testing.T) {
+	tests := []struct {
+		expected string
+		name     string
+		f        func(w io.Writer, s *aggregation.Status) error
+	}{
+		{
+			expected: expectedSummary,
+			name:     "StatusSummary",
+			f:        printSummary,
+		},
+		{
+			expected: expectedShowAll,
+			name:     "StatusShowAll",
+			f:        printAll,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			var b bytes.Buffer
+			err := test.f(&b, &exampleStatus)
+			if err != nil {
+				t.Fatalf("expected err to be nil, got %v", err)
+			}
+
+			if b.String() != test.expected {
+				t.Errorf("expected output to be %q, got %q", test.expected, b.String())
+			}
+		})
+	}
+}
