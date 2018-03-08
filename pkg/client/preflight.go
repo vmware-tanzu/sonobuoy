e46f80bed01742779683f3a2d6445eefb4325dde
Liz <33235082+liztio@users.noreply.github.com>
2018-03-08 15:28:04 -0500

    Sonobuoy existing preflight check (#345)
    Signed-off-by: liz <liz@heptio.com>

--- pkg/client/preflight.go
+++ pkg/client/preflight.go
@@ -22,17 +22,19 @@ import (
 	version "github.com/hashicorp/go-version"
 	"github.com/heptio/sonobuoy/pkg/buildinfo"
 	"github.com/pkg/errors"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 )
 
-var preflightChecks = []func(kubernetes.Interface) error{
+var preflightChecks = []func(kubernetes.Interface, *PreflightConfig) error{
 	preflightDNSCheck,
 	preflightVersionCheck,
+	preflightExistingSonobuoy,
 }
 
 // PreflightChecks runs all preflight checks in order, returning the first error encountered.
-func (c *SonobuoyClient) PreflightChecks() []error {
+func (c *SonobuoyClient) PreflightChecks(cfg *PreflightConfig) []error {
 	client, err := c.Client()
 	if err != nil {
 		return []error{err}
@@ -41,7 +43,7 @@ func (c *SonobuoyClient) PreflightChecks() []error {
 	errors := []error{}
 
 	for _, check := range preflightChecks {
-		if err := check(client); err != nil {
+		if err := check(client, cfg); err != nil {
 			errors = append(errors, err)
 		}
 	}
@@ -54,7 +56,7 @@ const (
 	kubeDNSLabelValue   = "kube-dns"
 )
 
-func preflightDNSCheck(client kubernetes.Interface) error {
+func preflightDNSCheck(client kubernetes.Interface, cfg *PreflightConfig) error {
 	selector := metav1.AddLabelToSelector(&metav1.LabelSelector{}, kubeDNSLabelKey, kubeDNSLabelValue)
 
 	obj, err := client.CoreV1().Pods(kubeSystemNamespace).List(
@@ -76,7 +78,7 @@ var (
 	maximumKubeVersion = version.Must(version.NewVersion(buildinfo.MaximumKubeVersion))
 )
 
-func preflightVersionCheck(client kubernetes.Interface) error {
+func preflightVersionCheck(client kubernetes.Interface, cfg *PreflightConfig) error {
 	versionInfo, err := client.Discovery().ServerVersion()
 	if err != nil {
 		return errors.Wrap(err, "failed to retrieve server version")
@@ -95,5 +97,20 @@ func preflightVersionCheck(client kubernetes.Interface) error {
 		return fmt.Errorf("Maximum kubernetes version is %s, got %s", maximumKubeVersion.String(), versionInfo.String())
 	}
 
+	return nil
+}
+
+func preflightExistingSonobuoy(client kubernetes.Interface, cfg *PreflightConfig) error {
+	_, err := client.CoreV1().Pods(cfg.Namespace).Get("sonobuoy", metav1.GetOptions{})
+	switch {
+	// Pod doesn't exist: great!
+	case apierrors.IsNotFound(err):
+		return nil
+	case err != nil:
+		return errors.Wrap(err, "error checking for Sonobuoy pod")
+	// No error: pod exists
+	case err == nil:
+		return errors.New("sonobuoy run already exists in this namespace")
+	}
 	return nil
 }
