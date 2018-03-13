9466e961305d150d03481ff011dbb81f8c61e608
liz <liz@heptio.com>
2018-03-13 11:45:52 -0400

    Move encryption/decryption into Travis so it won't choke on PRs
    Signed-off-by: liz <liz@heptio.com>

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 if [ -z "$TRAVIS" ]; then
     echo "this script is intended to be run only on travis" >&2;
     exit 1
@@ -8,5 +9,8 @@ fi
 if [ ! -z "$TRAVIS_TAG" ]; then
     curl -sL https://git.io/goreleaser | bash;
 fi
+
+openssl aes-256-cbc -K $encrypted_708bef23737d_key -iv $encrypted_708bef23737d_iv -in heptio-images-ee4b0474b93e.json.enc -out ./heptio-images-ee4b0474b93e.json -d
+gcloud auth activate-service-account --key-file heptio-images-ee4b0474b93e.json
 
 IMAGE_BRANCH="$TRAVIS_BRANCH" DOCKER="gcloud docker -- " make container push
