b76e05562286c79ae29fab1d302a5d78ca5b8e11
liz <liz@heptio.com>
2018-03-13 12:11:50 -0400

    Hopefully now we build both releaser and gcr on tag deploys
    Signed-off-by: liz <liz@heptio.com>
    
    Signed-off-by: Jesse Hamilton jesse.hamilton@heptio.com

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -2,15 +2,26 @@
 
 
 if [ -z "$TRAVIS" ]; then
-    echo "this script is intended to be run only on travis" >&2;
+    echo "this script is intended to be run only on travis" >&2
     exit 1
 fi
 
+function goreleaser() {
+    curl -sL https://git.io/goreleaser | bash
+}
+
+function gcr_push() {
+    openssl aes-256-cbc -K $encrypted_708bef23737d_key -iv $encrypted_708bef23737d_iv -in heptio-images-ee4b0474b93e.json.enc -out ./heptio-images-ee4b0474b93e.json -d
+    gcloud auth activate-service-account --key-file heptio-images-ee4b0474b93e.json
+    IMAGE_BRANCH="$TRAVIS_BRANCH" DOCKER="gcloud docker -- " make container push
+}
+
 if [ ! -z "$TRAVIS_TAG" ]; then
-    curl -sL https://git.io/goreleaser | bash;
+    goreleaser
+    gcr_push
 fi
 
-openssl aes-256-cbc -K $encrypted_708bef23737d_key -iv $encrypted_708bef23737d_iv -in heptio-images-ee4b0474b93e.json.enc -out ./heptio-images-ee4b0474b93e.json -d
-gcloud auth activate-service-account --key-file heptio-images-ee4b0474b93e.json
+if [ "$TRAVIS_BRANCH" == "master"]; then
+    gcr_push
+fi
 
-IMAGE_BRANCH="$TRAVIS_BRANCH" DOCKER="gcloud docker -- " make container push
