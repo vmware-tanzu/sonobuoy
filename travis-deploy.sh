ba6e7ac939fda92e491b919d11059a13c99fe8ee
liz <liz@heptio.com>
2018-07-26 14:53:52 -0400

    Fiddle with Travis a bit
    Signed-off-by: liz <liz@heptio.com>

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -13,7 +13,8 @@ function goreleaser() {
 function gcr_push() {
     openssl aes-256-cbc -K $encrypted_708bef23737d_key -iv $encrypted_708bef23737d_iv -in heptio-images-ee4b0474b93e.json.enc -out ./heptio-images-ee4b0474b93e.json -d
     gcloud auth activate-service-account --key-file heptio-images-ee4b0474b93e.json
-    IMAGE_BRANCH="$TRAVIS_BRANCH" DOCKER="gcloud docker -- " make container push
+    gcloud auth configure-docker
+    IMAGE_BRANCH="$TRAVIS_BRANCH" make container push
 }
 
 if [ ! -z "$TRAVIS_TAG" ]; then
