6b961d196a8bfaa1380f56bdeabafb55e78bc8b1
liz <liz@heptio.com>
2018-07-27 15:51:06 -0400

    Bail on error

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+set -e
 
 if [ -z "$TRAVIS" ]; then
     echo "this script is intended to be run only on travis" >&2
@@ -13,8 +14,7 @@ function goreleaser() {
 function gcr_push() {
     openssl aes-256-cbc -K $encrypted_708bef23737d_key -iv $encrypted_708bef23737d_iv -in heptio-images-ee4b0474b93e.json.enc -out ./heptio-images-ee4b0474b93e.json -d
     gcloud auth activate-service-account --key-file heptio-images-ee4b0474b93e.json
-    gcloud beta auth configure-docker --quiet
-    IMAGE_BRANCH="$TRAVIS_BRANCH" make container push
+    IMAGE_BRANCH="$TRAVIS_BRANCH" DOCKER="gcloud docker -- " make container push
 }
 
 if [ ! -z "$TRAVIS_TAG" ]; then
