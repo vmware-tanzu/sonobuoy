ea98f95fd582c88b7554be2f6e5172425d48817e
Bridget McErlean <bmcerlean@vmware.com>
2019-06-21 12:39:35 -0400

    Prepare v0.15.0 release
    Increase the version number and include the new images from the
    Kubernetes 1.15 release.
    
    Signed-off-by: Bridget McErlean <bmcerlean@vmware.com>

--- pkg/image/manifest.go
+++ pkg/image/manifest.go
@@ -88,6 +88,8 @@ func (r *RegistryList) GetImageConfigs() (map[string]Config, error) {
 			return r.v1_13(), nil
 		case 14:
 			return r.v1_14(), nil
+		case 15:
+			return r.v1_15(), nil
 		}
 	}
 	return map[string]Config{}, fmt.Errorf("No matching configuration for k8s version: %v", r.K8sVersion)
