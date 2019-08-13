af5e829e1008d73d4fac3f2e7a4089d594f9d51e
John Schnake <jschnake@vmware.com>
2019-08-13 14:18:59 -0500

    Add image building/pushing back to Travis
    Got authorization to re-add credentials for image pushing to
    Travis via their recommended approach outlined here:
    https://docs.travis-ci.com/user/encrypting-files/
    
    Fixes #808
    
    Signed-off-by: John Schnake <jschnake@vmware.com>

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -12,8 +12,15 @@ function goreleaser() {
     curl -sL https://git.io/goreleaser | bash
 }
 
-if [ ! -z "$TRAVIS_TAG" ]; then
+function gcr_push() {
+    openssl aes-256-cbc -K $encrypted_222a2009ef6d_key -iv $encrypted_222a2009ef6d_iv -in heptio-images-c14f11347d8b.json.enc -out heptio-images-c14f11347d8b.json -d
+    gcloud auth activate-service-account --key-file heptio-images-c14f11347d8b.json
+    # https://github.com/travis-ci/travis-ci/issues/9905
+    unset GIT_HTTP_USER_AGENT
+    IMAGE_BRANCH="$BRANCH" DOCKER="gcloud docker -- " make container push
+}
 
+if [ ! -z "$TRAVIS_TAG" ]; then
     if [ "$(./sonobuoy version --short)" != "$TRAVIS_TAG" ]; then
         echo "sonobuoy version does not match tagged version!" >&2
         echo "sonobuoy short version is $(./sonobuoy version --short)" >&2
@@ -23,4 +30,9 @@ if [ ! -z "$TRAVIS_TAG" ]; then
     fi
 
     goreleaser --skip-validate
+    gcr_push
+fi
+
+if [ "$BRANCH" == "master" ]; then
+    gcr_push
 fi
