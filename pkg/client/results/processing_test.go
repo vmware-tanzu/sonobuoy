0a17d7e5a7d85c8573a131591a8a2c73d46335ae
John Schnake <jschnake@vmware.com>
2019-09-12 13:41:16 -0500

    Update contributor copyright
    Signed-off-by: John Schnake <jschnake@vmware.com>

--- pkg/client/results/processing_test.go
+++ pkg/client/results/processing_test.go
@@ -1,5 +1,5 @@
 /*
-Copyright the Sonobuoy contributors 2019
+Copyright the Sonobuoy project contributors 2019
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
--- pkg/discovery/queries.go
+++ pkg/discovery/queries.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2018 Heptio Inc.
+Copyright 2018 the Sonobuoy project contributors
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
