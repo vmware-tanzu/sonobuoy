bf2fa52377ad3dc8fd3ffc5aac4efc8ab21037fb
Bridget McErlean <bmcerlean@vmware.com>
2019-09-27 17:21:42 -0400

    Add a small test which runs a basic plugin (#903)
    This change also modifies our Makefile and CI scripts so that the tests
    can be run on a separate kind cluster.
    
    Signed-off-by: Bridget McErlean <bmcerlean@vmware.com>

---
+++ scripts/run_integration_tests.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+SCRIPTS_DIR="$( cd "$( dirname "$0" )" >/dev/null 2>&1 && pwd )"
+DIR=$(cd $SCRIPTS_DIR; cd ..; pwd)
+
+cluster="integration"
+testImage="sonobuoy/testimage:v0.1"
+
+if ! kind get clusters | grep -q "^$cluster$"; then
+    kind create cluster --name $cluster
+    # Although the cluster has been created, not all the pods in kube-system are created/available
+    sleep 20
+fi
+
+# Build and load the test plugin image
+make -C $DIR/test/integration/testImage
+kind load docker-image --name $cluster $testImage
+
+# Build and load the sonobuoy image and run integration tests
+make -C $DIR KIND_CLUSTER=$cluster deploy_kind
+KUBECONFIG="$(kind get kubeconfig-path --name="$cluster")" VERBOSE=true make -C $DIR int
