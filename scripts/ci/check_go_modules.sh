94cf37210cba7eb7ca0db60415351791ff72e038
Bridget McErlean <bmcerlean@vmware.com>
2019-10-07 14:45:42 -0400

    Add CircleCI config
    This initial config replicates some of the steps from our TravisCI
    config. It introduces a new workflow that contains 3 jobs. It currently
    only handles the steps that take place on PR branches:
    
    * Checking that the READMEs are in sync
    * Checking the go module state
    * Running unit, stress, integration tests and the single Sonobuoy run
    
    It does not yet handle image pushing. That will be addressed in a later
    PR.
    
    Signed-off-by: Bridget McErlean <bmcerlean@vmware.com>

---
+++ scripts/ci/check_go_modules.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+go mod tidy
+
+git_status=$(git status -s)
+if [ -n "$git_status" ]; then
+    echo $git_status
+    echo "go mod tidy modified the git status; did you need add/remove a dependency?"
+    exit 1
+fi
