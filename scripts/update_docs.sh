6a933dd142c2a3c7b5718166173d20c115e2436e
Wilson E. Husin <whusin@vmware.com>
2021-03-18 13:47:09 -0700

    Update docs with Hugo paths
    Signed-off-by: Wilson E. Husin <whusin@vmware.com>

--- scripts/update_docs.sh
+++ scripts/update_docs.sh
@@ -8,16 +8,16 @@ VERSION=$1
 TOC_NAME=$(echo ${VERSION}toc|sed s/\\./-/g)
 
 read -r -d '' CONFIG_VERSION_BLOCK << EOM
-latest: v.*
-versions:
-- master
+  docs_latest: v.*
+  docs_versions:
+    - master
 EOM
 
 read -r -d '' NEW_VERSION_BLOCK << EOM
-latest: ${VERSION}
-versions:
-- master
-- ${VERSION}
+  docs_latest: ${VERSION}
+  docs_versions:
+    - master
+    - ${VERSION}
 EOM
 
 read -r -d '' OLD_SCOPE_BLOCK << EOM
@@ -44,6 +44,24 @@ read -r -d '' NEW_SCOPE_BLOCK << EOM
       layout: \"docs\"
 EOM
 
+read -r -d '' MASTER_FRONTMATTER << EOM
+---
+version: master
+cascade:
+  layout: docs
+  gh: https:\/\/github.com\/vmware-tanzu\/sonobuoy\/tree\/.*
+---
+EOM
+
+read -r -d '' RELEASE_FRONTMATTER << EOM
+---
+version: ${VERSION}
+cascade:
+  layout: docs
+  gh: https:\/\/github.com\/vmware-tanzu\/sonobuoy\/tree\/${VERSION}
+---
+EOM
+
 OLD_TOC_BLOCK="master: master-toc"
 read -r -d '' NEW_TOC_BLOCK << EOM
 master: master-toc
@@ -61,16 +79,17 @@ else
         -v ${DIR}:/root \
         debian:stretch-slim \
         /bin/sh -c \
-        "rm -rf /root/site/docs/${VERSION} && \
-        cp -r /root/site/docs/master /root/site/docs/${VERSION} && \
-        sed -i 's/site\/docs\/master\///g' /root/site/docs/${VERSION}/README.md && \
-        sed -i 's/docs\/img/img/g' /root/site/docs/${VERSION}/README.md && \
-        sed -i 's/sonobuoy\/tree\/master/sonobuoy\/tree\/${VERSION}/g' /root/site/docs/${VERSION}/README.md && \
-        sed -i 's/sonobuoy.io\/docs\/master/sonobuoy.io\/docs\/${VERSION}/g' /root/site/docs/${VERSION}/README.md && \
-        cp /root/site/_data/master-toc.yml /root/site/_data/${TOC_NAME}.yml && \
-        perl -i -0pe 's/${CONFIG_VERSION_BLOCK}/${NEW_VERSION_BLOCK}/' /root/site/_config.yml && \
-        perl -i -0pe 's/${OLD_SCOPE_BLOCK}/${NEW_SCOPE_BLOCK}/' /root/site/_config.yml && \
-        perl -i -0pe 's/${OLD_TOC_BLOCK}/${NEW_TOC_BLOCK}/' /root/site/_data/toc-mapping.yml && \
-        sed -i 's/\/docs\/v.*\"/\/docs\/${VERSION}\"/' /root/site/_includes/site-header.html && \
-        sed -i 's/${OLD_FOOTER_BLOCK}/${NEW_FOOTER_BLOCK}/' /root/site/_config.yml"
+        "rm -rf /root/site/content/docs/${VERSION} && \
+        cp -r /root/site/content/docs/master /root/site/content/docs/${VERSION} && \
+        sed -i 's/site\/docs\/master\///g' /root/site/content/docs/${VERSION}/_index.md && \
+        sed -i 's/docs\/img/img/g' /root/site/content/docs/${VERSION}/_index.md && \
+        sed -i 's/sonobuoy\/tree\/master/sonobuoy\/tree\/${VERSION}/g' /root/site/content/docs/${VERSION}/_index.md && \
+        sed -i 's/sonobuoy.io\/docs\/master/sonobuoy.io\/docs\/${VERSION}/g' /root/site/content/docs/${VERSION}/_index.md && \
+        cp /root/site/data/docs/master-toc.yml /root/site/data/docs/${TOC_NAME}.yml && \
+        perl -i -0pe 's/${CONFIG_VERSION_BLOCK}/${NEW_VERSION_BLOCK}/' /root/site/config.yaml && \
+        perl -i -0pe 's/${OLD_SCOPE_BLOCK}/${NEW_SCOPE_BLOCK}/' /root/site/config.yaml && \
+        perl -i -0pe 's/${OLD_TOC_BLOCK}/${NEW_TOC_BLOCK}/' /root/site/data/docs/toc-mapping.yml && \
+        perl -i -0pe 's/${MASTER_FRONTMATTER}/${RELEASE_FRONTMATTER}/' /root/site/content/docs/${VERSION}/index-frontmatter.yaml && \
+        perl -i -0pe 's/${MASTER_FRONTMATTER}/${RELEASE_FRONTMATTER}/' /root/site/content/docs/${VERSION}/_index.md && \
+        sed -i 's/${OLD_FOOTER_BLOCK}/${NEW_FOOTER_BLOCK}/' /root/site/config.yaml"
 fi
