346d26e76ad03d8ec08baa0dc1baf16d3b4e2e09
John Schnake <jschnake@vmware.com>
2021-09-22 13:03:49 -0500

    Default our experimental features to true
    Added logic so users can turn them off if it breaks
    their logic in some way.
    
    Fixes #1436
    Fixes #1437
    
    Signed-off-by: John Schnake <jschnake@vmware.com>

--- cmd/sonobuoy/app/featureGates.go
+++ cmd/sonobuoy/app/featureGates.go
@@ -10,6 +10,24 @@ const (
 	FeatureWaitOutputProgressByDefault = "SONOBUOY_WAIT_PROGRESS"
 )
 
+var (
+	featureDefaultMap = map[string]bool{
+		FeaturePluginInstallation:          true,
+		FeatureWaitOutputProgressByDefault: true,
+	}
+)
+
+// featureEnabled returns if the named feature is enabled based on the current env and defaults.
 func featureEnabled(feature string) bool {
-	return os.Getenv(FeaturesAll) == "true" || os.Getenv(feature) == "true"
+	return featureEnabledCore(feature, os.Getenv(FeaturesAll), os.Getenv(feature), featureDefaultMap)
+}
+
+// Extracted logic here for testing so we can modify the env and defaults easily.
+func featureEnabledCore(featureName, allEnv, featureEnv string, defaultMap map[string]bool) bool {
+	// Allow features we default as true to be turned off while still relatively new so if major
+	// bugs are found we have workarounds.
+	if featureEnv == "false" {
+		return false
+	}
+	return defaultMap[featureName] || allEnv == "true" || featureEnv == "true"
 }
