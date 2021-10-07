0e55157cd9a15f9e44de9bcd0f3c1860688b5eb3
John Schnake <jschnake@vmware.com>
2021-10-07 10:52:33 -0500

    Combine gen and run flags
    Moves wait/wait-output/skip-preflight to the gen
    flag set an not just run's. This allows gen and run to
    both process all the flags so that you can use `gen` to
    test any `run` command without having to add/remove flags
    when you go back and forth.
    
    The new values do not impact gen in any way unless you provide
    the `-f` flag, in which case gen will just print the file.
    
    Signed-off-by: John Schnake <jschnake@vmware.com>

--- cmd/sonobuoy/app/run.go
+++ cmd/sonobuoy/app/run.go
@@ -18,66 +18,18 @@ package app
 
 import (
 	"fmt"
-	"os"
-	"time"
-
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
+	"os"
 
 	"github.com/vmware-tanzu/sonobuoy/pkg/client"
 	"github.com/vmware-tanzu/sonobuoy/pkg/errlog"
 )
 
-type runFlags struct {
-	genFlags
-	skipPreflight bool
-	wait          int
-	waitOutput    WaitOutputMode
-	genFile       string
-}
-
 var (
 	allowedGenFlagsWithRunFile = []string{kubeconfig, kubecontext}
 )
-
-func RunFlagSet(cfg *runFlags) *pflag.FlagSet {
-	runset := pflag.NewFlagSet("run", pflag.ExitOnError)
-	// Default to detect since we need kubeconfig regardless
-	runset.AddFlagSet(GenFlagSet(&cfg.genFlags, DetectRBACMode))
-	AddSkipPreflightFlag(&cfg.skipPreflight, runset)
-	AddRunWaitFlag(&cfg.wait, runset)
-	if featureEnabled(FeatureWaitOutputProgressByDefault) {
-		AddWaitOutputFlag(&cfg.waitOutput, runset, ProgressOutputMode)
-	} else {
-		AddWaitOutputFlag(&cfg.waitOutput, runset, SilentOutputMode)
-	}
-
-	runset.StringVarP(
-		&cfg.genFile, "file", "f", "",
-		"If set, loads the file as if it were the output from sonobuoy gen. Set to `-` to read from stdin.",
-	)
-
-	return runset
-}
-
-func (r *runFlags) Config() (*client.RunConfig, error) {
-	runcfg := &client.RunConfig{
-		Wait:       time.Duration(r.wait) * time.Minute,
-		WaitOutput: r.waitOutput.String(),
-		GenFile:    r.genFile,
-	}
-
-	if r.genFile == "" {
-		gencfg, err := r.genFlags.Config()
-		if err != nil {
-			return nil, err
-		}
-		runcfg.GenConfig = *gencfg
-	}
-
-	return runcfg, nil
-}
 
 func givenAnyGenConfigFlags(fs *pflag.FlagSet, allowedFlagNames []string) bool {
 	changed := false
@@ -93,8 +45,8 @@ func givenAnyGenConfigFlags(fs *pflag.FlagSet, allowedFlagNames []string) bool {
 }
 
 func NewCmdRun() *cobra.Command {
-	var f runFlags
-	fs := RunFlagSet(&f)
+	var f genFlags
+	fs := GenFlagSet(&f, DetectRBACMode)
 	cmd := &cobra.Command{
 		Use:   "run",
 		Short: "Starts a Sonobuoy run by launching the Sonobuoy aggregator and plugin pods.",
@@ -109,14 +61,14 @@ func NewCmdRun() *cobra.Command {
 	return cmd
 }
 
-func checkFlagValidity(fs *pflag.FlagSet, rf runFlags) error {
+func checkFlagValidity(fs *pflag.FlagSet, rf genFlags) error {
 	if rf.genFile != "" && givenAnyGenConfigFlags(fs, allowedGenFlagsWithRunFile) {
 		return fmt.Errorf("setting the --file flag is incompatible with any other options besides %v", allowedGenFlagsWithRunFile)
 	}
 	return nil
 }
 
-func submitSonobuoyRun(f *runFlags) func(cmd *cobra.Command, args []string) {
+func submitSonobuoyRun(f *genFlags) func(cmd *cobra.Command, args []string) {
 	return func(cmd *cobra.Command, args []string) {
 		sbc, err := getSonobuoyClientFromKubecfg(f.kubecfg)
 		if err != nil {
@@ -124,7 +76,7 @@ func submitSonobuoyRun(f *runFlags) func(cmd *cobra.Command, args []string) {
 			os.Exit(1)
 		}
 
-		runCfg, err := f.Config()
+		runCfg, err := f.RunConfig()
 		if err != nil {
 			errlog.LogError(errors.Wrap(err, "could not retrieve E2E config"))
 			os.Exit(1)
