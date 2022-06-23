e36a26ce82c0bd72212819f43e736479accd9664
Tanmay Godbole <tanmay.godbole@nokia.com>
2022-06-23 12:53:03 +0300

    Update copyright and golden files
    Signed-off-by: Tanmay Godbole <tanmay.godbole@nokia.com>

--- cmd/sonobuoy/app/args.go
+++ cmd/sonobuoy/app/args.go
@@ -1,7 +1,7 @@
 /*
 Copyright 2018 Heptio Inc.
 
-© 2022 Nokia
+Copyright 2022 the Sonobuoy Project contributors
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
