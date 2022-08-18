0ab80f7bc30e943ffe056ef52364b0f801aeff6a
John Schnake <jschnake@vmware.com>
2022-08-18 10:07:05 -0500

    Add new data from k8s releases for e2e command
    Signed-off-by: John Schnake <jschnake@vmware.com>

--- scripts/gather_e2e_data.sh
+++ scripts/gather_e2e_data.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -ex
 
+# Note: I've experienced flakiness here before where the files came over but had 0 data; so I think it was a small race
+# between some of these commands somehow. If necessary, you can run them manually and confirm they have the expected data.
 sonobuoy run -p ./gathere2e/gathere2e.yaml --wait
 sonobuoy retrieve -x tmpoutput
 cp -f tmpoutput/plugins/gathere2e/results/global/* ../cmd/sonobuoy/app/e2e/testLists
@@ -8,5 +10,7 @@ cp -f tmpoutput/plugins/gathere2e/results/global/* ../cmd/sonobuoy/app/e2e/testL
   cd ../cmd/sonobuoy/app/e2e/testLists/
   find . -regex '.*[0-9]$' -exec gzip -f {} \;
 )
-sonobuoy delete
-rm -rf tmpoutput
+
+# Uncomment if you want cleanup to automatically occur rather than be manual.
+#sonobuoy delete
+#rm -rf tmpoutput
